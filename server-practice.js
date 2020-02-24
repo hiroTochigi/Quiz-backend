9059e681b2f04606e2f3925dc52422bb83c46b28
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2020-02-23 20:40:07 -0600

    restfulAPI

--- server-practice.js
+++ server-practice.js
@@ -1,7 +1,6 @@
 const express = require("express");
- 
-const app = express();
 
+const app = express();
 
 //midlleware
 app.use((req, res, next) =>{
@@ -14,9 +13,11 @@ app.use(express.urlencoded({extended:false}))
 //middleware for json file
 app.use(express.json())
 
+app.use(express.static(__dirname + "/public"))
+
 
 //get request in root to get html
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
     res.send("<h1>hello</h1>");
 })
 
@@ -31,8 +32,19 @@ app.get("/profile/", (req, res) => {
 
 //post request in profile
 app.post("/profile/", (req, res) => {
-    console.log(req.body)
+    console.log(req.body) //req.body is used to send form data or json data
 })
 
+app.get("/query/", (req, res) =>{
+    console.log(req.query) //url?name=hiroyuki
+    console.log(req.headers)
+    console.log(req.params)
+    res.send("success")
+})
+
+app.get("/params/:id", (req, res) =>{
+    console.log(req.params)
+    res.status(404).send("not found")
+})
 
 app.listen(3000);
