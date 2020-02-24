3c122fbae0cdf6d692bd56e8e7a937b78bc42a1e
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2020-02-23 20:02:14 -0600

    server practice 1

--- server-practice.js
+++ server-practice.js
@@ -1,30 +1,37 @@
 const express = require("express");
-
+ 
 const app = express();
+
 
 //midlleware
 app.use((req, res, next) =>{
     console.log("hello")
     next()
 })
+//middleware for form data
+app.use(express.urlencoded({extended:false}))
 
-//get request in root
+//middleware for json file
+app.use(express.json())
+
+
+//get request in root to get html
 app.get("/", (req, res) => {
     res.send("<h1>hello</h1>");
 })
 
-//get request in profile
+//get request in profile to get json file
 app.get("/profile/", (req, res) => {
-    res.send("get profile");
-})
-
-//post request in profile
-app.post("/profile/", (req, res) => {
     const user = {
         name: "Sarah",
         hobby: "1Life1Hour"
     }
     res.send(user)
+})
+
+//post request in profile
+app.post("/profile/", (req, res) => {
+    console.log(req.body)
 })
 
 
